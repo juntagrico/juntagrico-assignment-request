7865658b9400a878719b7d6c982d268fb0980c36
David Glenck <pascal.free@ihdg.ch>
2020-07-13 09:38:30 +0200

    release 0.1.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='juntagrico_assignment_request',
-    version='0.1.0dev',
+    version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
     license='LPGLv3',  # example license
