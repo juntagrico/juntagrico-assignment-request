b8c2ee06e7f7f0d519acb56f7f33242d7a093932
David Glenck <pascal.free@ihdg.ch>
2020-07-13 09:39:48 +0200

    fast fix for juntagrico 1.2.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='juntagrico_assignment_request',
-    version='0.1.0',
+    version='0.2.0dev',
     packages=find_packages(),
     include_package_data=True,
     license='LPGLv3',  # example license
